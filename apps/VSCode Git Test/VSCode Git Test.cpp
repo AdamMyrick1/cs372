58dce582949dedd2992c2f0ba7bc09fff1687132
Adam Myrick <amyrick2@my.athens.edu>
2023-06-13 18:53:04 -0500

    Testing VSCode Git feature

--- apps/VSCode Git Test/VSCode Git Test.cpp
+++ apps/VSCode Git Test/VSCode Git Test.cpp
@@ -7,6 +7,7 @@ int main ()
 {
     cout << "This program adds 1 to a number!" << endl;
     addone number(1);
-    cout << "Number incremented by one is: " << number.incrementNumber();
+    cout << "Number incremented by one is: " << number.incrementNumber() << endl;
+    cout <<"Goodbye!";
     return 0;
 }
